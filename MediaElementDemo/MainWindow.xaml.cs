133b3e9be9092b8e39065f18240ae16e587b5564
Jacob Zimmerman <D00425031@utahtech.edu>
2022-11-15 11:41:34 -0700

    SkipTimes is working, things polished, may still need another creative addition.

--- MediaElementDemo/MainWindow.xaml.cs
+++ MediaElementDemo/MainWindow.xaml.cs
@@ -13,34 +13,63 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+using System.IO;
+
 namespace MediaElementDemo
 {
     public partial class MainWindow : Window
     {
+        bool mIsPlaying = false;
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+        private string[] lines;
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
-        /*
+        
         private void SkipBlockedTimes()
         {
             // if the mediaElement is between forbidden times, skip past it
             for (int i =0; i < timesCombo.Items.Count; i++)
             {
                 string line = timesCombo.Items[i].ToString();
-                string[] words = line.Split(" ");
+                string[] words = line.Split(' ');
                 double cut = Double.Parse(words[0]);
                 double restart = Double.Parse(words[1]);
                 //double cut = TimesStringToDouble(words[0]);
                 //double restart - TimeStringToDouble(words[1]);
-                if (MediaPlayer.Position.TotalSeconds >= cut 
-                    && MediaPlayer.Position.TotalSeconds < restart)
+                if (Media.Position.TotalSeconds >= cut 
+                    && Media.Position.TotalSeconds < restart)
                 {
-                    MediaPlayer.Position = TimeSpan.FromSeconds(restart);
+                    Media.Position = TimeSpan.FromSeconds(restart);
                 }
             }
         }
-        */
+
+        private void LoadSkipped_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Choose .txt file";
+            if (dialog.ShowDialog() == true)
+            {
+                timesCombo.Items.Clear();
+                lines = File.ReadAllLines(dialog.FileName, Encoding.UTF8);
+                for (int i = 0; i <= lines.Count() - 1; i++)
+                {
+                    timesCombo.Items.Add(lines[i]);
+                }
+            }
+        }
+
+
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog();
@@ -55,7 +84,18 @@ namespace MediaElementDemo
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             if (Media.Source != null)
-                Media.Play();
+                if (!mIsPlaying)
+                {
+                    Media.Play();
+                    mIsPlaying = true;
+                    play_pause_text.Text = "Pause";
+                } else
+                {
+                    Media.Pause();
+                    mIsPlaying = false;
+                    play_pause_text.Text = "Play";
+                }
+                    
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
@@ -73,6 +113,18 @@ namespace MediaElementDemo
         private void MuteButton_Click(object sender, RoutedEventArgs e)
         {
             Media.IsMuted = !Media.IsMuted;
+            if (Media.IsMuted) MuteButton.Content = "UnMute";
+            else MuteButton.Content = "Mute";
+        }
+
+        private void BackwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            Media.Position = Media.Position.Add(TimeSpan.FromSeconds(-10));
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            Media.Position = Media.Position.Add(TimeSpan.FromSeconds(10));
         }
 
         private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -88,6 +140,30 @@ namespace MediaElementDemo
         private void Speed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             Media.SpeedRatio = SpeedSlider.Value;
+        }
+        private void Progress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void Progress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            Media.Position = TimeSpan.FromSeconds(VideoSlider.Value);
+        }
+        private void Progress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            time_display.Text = TimeSpan.FromSeconds(VideoSlider.Value).ToString(@"hh\:mm\:ss");
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((Media.Source != null) && Media.NaturalDuration.HasTimeSpan && (!userIsDraggingSlider))
+            {
+                VideoSlider.Minimum = 0;
+                VideoSlider.Maximum = Media.NaturalDuration.TimeSpan.TotalSeconds;
+                VideoSlider.Value = Media.Position.TotalSeconds;
+                SkipBlockedTimes();
+            }
         }
     }
 }
